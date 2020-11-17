b21b036320070010e7f43c3aa14e9ee001e5251e
Brent Gorwin <brent.gorwin@gmail.com>
2020-11-16 21:05:35 -0600

    finished the sidebar of library music list and the toggle state button to slide the menu in and out

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 // Import styles
 import './styles/app.scss';
 
 // Import Components
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from './components/Library';
+import Nav from './components/Nav';
 
 // Import Util
 import data from './util';
 
 function App() {
+	//Ref
+	const audioRef = useRef(null);
+
 	// State
 	const [songs, setSongs] = useState(data());
 	const [currentSong, setCurrentSong] = useState(songs[0]);
 	const [isPlaying, setIsPlaying] = useState(false);
+	const [songInfo, setSongInfo] = useState({
+		currentTime: 0,
+		duration: 0
+	});
+	const [libraryStatus, setLibraryStatus] = useState(false);
+
+	const timeUpdateHandler = (e) => {
+		const current = e.target.currentTime;
+		const duration = e.target.duration;
+		setSongInfo({ ...songInfo, currentTime: current, duration });
+	};
 
 	return (
 		<div className='App'>
+			<Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
 			<Song currentSong={currentSong} />
 			<Player
+				audioRef={audioRef}
 				isPlaying={isPlaying}
 				setIsPlaying={setIsPlaying}
 				currentSong={currentSong}
+				setSongInfo={setSongInfo}
+				songInfo={songInfo}
 			/>
+			<Library
+				songs={songs}
+				setCurrentSong={setCurrentSong}
+				audioRef={audioRef}
+				isPlaying={isPlaying}
+				setSongs={setSongs}
+				libraryStatus={libraryStatus}
+			/>
+			<audio
+				onTimeUpdate={timeUpdateHandler}
+				onLoadedMetadata={timeUpdateHandler}
+				ref={audioRef}
+				src={currentSong.audio}
+			></audio>
 		</div>
 	);
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
 	faPlay,
@@ -7,10 +7,14 @@ import {
 	faPause
 } from '@fortawesome/free-solid-svg-icons';
 
-const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
-	//Ref
-	const audioRef = useRef(null);
-
+const Player = ({
+	audioRef,
+	currentSong,
+	isPlaying,
+	setIsPlaying,
+	setSongInfo,
+	songInfo
+}) => {
 	// Event Handlers
 	const playSongHandler = () => {
 		if (isPlaying) {
@@ -27,23 +31,11 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 		setSongInfo({ ...songInfo, currentTime: e.target.value });
 	};
 
-	const timeUpdateHandler = (e) => {
-		const current = e.target.currentTime;
-		const duration = e.target.duration;
-		setSongInfo({ ...songInfo, currentTime: current, duration });
-	};
-
 	const getTime = (time) => {
 		return (
 			Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2)
 		);
 	};
-
-	// State
-	const [songInfo, setSongInfo] = useState({
-		currentTime: 0,
-		duration: 0
-	});
 
 	return (
 		<div className='player'>
@@ -51,7 +43,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 				<p>{getTime(songInfo.currentTime)}</p>
 				<input
 					min={0}
-					max={songInfo.duration}
+					max={songInfo.duration || 0}
 					value={songInfo.currentTime}
 					onChange={dragHandler}
 					type='range'
@@ -72,12 +64,6 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 					icon={faAngleRight}
 				/>
 			</div>
-			<audio
-				onTimeUpdate={timeUpdateHandler}
-				onLoadedMetadata={timeUpdateHandler}
-				ref={audioRef}
-				src={currentSong.audio}
-			></audio>
 		</div>
 	);
 };
